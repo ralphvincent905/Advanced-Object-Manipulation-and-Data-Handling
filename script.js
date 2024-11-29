acd50616ab2ded7a7dc8e4d27e2c79b407f698ed
ralphvincent905 <ralph_sampaga@yahoo.com>
2024-11-29 18:46:17 -0500

    part 4 spread operator/add new propety graduationYear

--- script.js
+++ script.js
@@ -33,3 +33,7 @@ const scores = [85, 92, 78, 90];
 const [score1, score2] = scores;
 console.log("First Score:", score1);
 console.log("Second Score:", score2);
+
+// Part 4: The Spread Operator
+const clonedStudent = { ...student, graduationYear: 2025 };
+console.log("Cloned Student with New Property:", clonedStudent);
