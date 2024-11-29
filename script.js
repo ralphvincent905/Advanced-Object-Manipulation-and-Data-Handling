f4f25972b556ef8bac8fe70099004734733d85ff
ralphvincent905 <ralph_sampaga@yahoo.com>
2024-11-29 18:34:19 -0500

    Add part 2 script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Part 1: Understanding and Creating Objects
 const student = {
     name: "Ralph Sampaga",
     age: 30,
@@ -12,3 +13,12 @@ console.log("Name:", student.name);
 console.log("Age:", student.age);
 
 console.log(student.displayInfo());
+
+// Part 2: Working with JSON
+const studentJSON = JSON.stringify(student);
+console.log("JSON String:", studentJSON);
+
+const newStudent = JSON.parse(studentJSON);
+console.log("Newly Created Object:", newStudent);
+
+console.log("Are the objects equal?", JSON.stringify(student) === JSON.stringify(newStudent));
